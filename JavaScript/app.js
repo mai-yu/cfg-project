701f7e94c316fe3471addf549659703f4972dcd7
maryamd13 <63812899+maryamd13@users.noreply.github.com>
2020-06-08 19:26:48 +0100

    Add files via upload

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,47 +1,72 @@
-document.getElementsByClassName("button1").onclick = button1;
+const cards = document.querySelectorAll(".memory-card")
+//const resultDisplay = document.querySelector ( "#result");
+let hasFlippedCard= false
+let lockBoard = false
+let firstCard, secondCard;
 
-function button1() {
-var Clue1 = prompt("On the chopping bench in the kitchen there are two items which could be used as evidence. One item provides direct evidence and you will be able to continue to the next stage straight away - the others will require you to solve a puzzle first...which do you choose 1 or 2?");
+function flipCard (){
+    if (lockBoard) return;
+    if (this==firstCard) return
+    ;
 
-if (Clue1==1) {
-  var Invoice = alert("You chose option 1, an old invoice! Hover the mouse over the hanging durian fruit for further instructions");
-}
-else {
-    alert("You chose option 2, an engraved chopping board! Well done - the engraving on the chopping board is a Chinese name - 张小平 (Zhang Xiaoping) - which is the name of the chef. This will be vital evidence! Remember the name...");
-    alert('Hover over the red lanterns for another clue.')
-}
-}
-    
-document.getElementsByClassName("button2").onclick = button2;
-function button2() {
-  var Police = alert("You think you've found the three peices of information needed? Lets see...");
-  var Answer1 = prompt("Where were the fortune cookies brought from?");
-  if (Answer1.toLowerCase() =='lucky cat casino') {
-    var Answer2 = prompt("What was the name of the chef?");
-  }
-    else {
-      alert('Missed a clue? Click on the picture of the panda to work out where the fortune cookies came from.');
+    this.classList.add("flip");
+    if (!hasFlippedCard) {
+        //first click
+        hasFlippedCard = true
+        firstCard= this;
+
+        return;
+    } 
+        //second click
+        hasFlippedCard = false;
+        secondCard = this;
+
+        checkForMatch ()
+        ;
     }
 
-  if (Answer2.toLowerCase() =='zhang xiaoping') {
-    var Answer3 = prompt("What time was the delivery on Sunday?"); 
-  }
-    else {
-    alert('Missed a clue? Click on the chopping board to work out the chefs name.');
-  }
+function checkForMatch (){ 
+    let isMatch =firstCard.dataset.framework==
+      secondCard.dataset.framework;
+      isMatch? disableCards(alert ("You found a match!")):unFlipCards()
 
-  if (Answer3.toLowerCase() =='4pm') {
-      var FinalList = alert(`You collected all the clues and reported relevant information to the police! You discovered that the chef, ${Answer2}, was expecting a delivery of drugs from ${Answer1} on Sunday at ${Answer3}. The police have an operation planned to arrest those involved when the delivery takes place thanks to you!`);
-    }
-    else {
-      alert("Hmmmm check the note from the casino again.");
-    }
-  }
-     
-    
+}
+//resultDisplay.textContent= cardsWon.length
+//if (cardsWon.length == cardsArray.length/2){
+    //resultDisplay.testContent = "Congratulations! You found them all!"
+//}
 
+function disableCards (){
 
-  
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);              
+        
+ resetBoard();
+}
+function unFlipCards(){
+    lockBoard = true;
 
+    setTimeout (( ) =>  {   
+        firstCard.classList.remove("flip")
+        secondCard.classList.remove("flip")
+        lockBoard = false;
+    }, 1500);
 
+}
+function resetBoard ( ){
+[hasFlippedCard, lockBoard]= [false,false];
+[firstCard, secondCard]= [nul,nul];
+}
 
+(function shuffle() {
+    cards.forEach(card => {
+        let randomPos = Math.floor (Math.random ()* 12)
+        card.style.order = randomPos;
+
+    });
+})( );
+
+//the extra parentheses made the function an immediate expression function which means it will be executed right after its definition.
+
+cards.forEach (card => card.addEventListener ("click", flipCard))
+;
